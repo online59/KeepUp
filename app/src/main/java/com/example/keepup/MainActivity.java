dfad1aa2ae460272852d918574afd6d809cd384f
online59 <taez.khamrangsi@gmail.com>
2023-04-21 13:27:16 +0700

    Add new project

--- app/src/main/java/com/example/keepup/MainActivity.java
+++ app/src/main/java/com/example/keepup/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.keepup;
 
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.keepup.di.RepositoryImpl;
+import com.example.keepup.entity.Task;
+import com.example.keepup.service.FirebaseService;
+import com.example.keepup.viewmodel.TaskViewModel;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button addTaskButton;
+    private TaskViewModel viewModel;
+    private Task task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setup();
+
+        addTaskButton.setOnClickListener(onClickListener);
+    }
+
+    private final View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            viewModel.addTask(task);
+        }
+    };
+
+    private void setup() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("task");
+        addTaskButton = findViewById(R.id.add_task_button);
+        viewModel = new TaskViewModel(new RepositoryImpl(new FirebaseService(reference)));
+
+        task.setTaskId(1);
+        task.setPreTaskId(0);
+        task.setNexTaskId(99);
+        task.setTitle("New Task");
+        task.setDeadline(122561f);
+        task.setStartDate(18536f);
+        task.setStatus(0);
+        task.setImportant(true);
+        task.setUrgent(true);
+        task.setDetail("New task added");
     }
 }
